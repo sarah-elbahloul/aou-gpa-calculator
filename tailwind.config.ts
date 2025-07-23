fef9cfd78dc5ddbfa6698cbc07576e3dc2964643
Sarah Elbahloul <sa.albahloul@gmail.com>
2025-07-23 20:41:05 +0300

    trying to deploy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
