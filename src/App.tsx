ee65b7055d2cb06dfad24ede5d8d7b6ffe8637c2
Sarah Elbahloul <sa.albahloul@gmail.com>
2025-07-23 21:04:00 +0300

    fixed that deployed did not worked, but need to fix that calls to firestore still dont work

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
@@ -6,12 +6,14 @@ import { TooltipProvider } from "./components/ui/tooltip";
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/aou-gpa-calculator">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -20,7 +22,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRouter />
       </TooltipProvider>
     </QueryClientProvider>
   );
